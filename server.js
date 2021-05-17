6b7ff0ee4bc735a2937be99cb6455d648a987df2
roughitsrinivasan <81728997+roughitsrinivasan@users.noreply.github.com>
2021-05-17 09:54:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.get("/",function(req, res){
+  res.sendFile(C:\Users\Roughit srinivasan\calculator +"index.html");
+  console.log(C:\Users\Roughit srinivasan\calculator);
+});
+
+
+
+app.listen(3000, function(){
+  console.log("Server has started on port 3000");
+});
